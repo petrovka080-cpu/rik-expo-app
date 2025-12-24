396bec80fc034c6b767be8f8e5cac5180c842aa7
Aziz <aziz@example.com>
2025-12-24 15:00:42 +0600

    chore: stabilize expo web, fix metro OOM, add expo-asset

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,14 @@
-﻿const { getDefaultConfig } = require("expo/metro-config");
+﻿// metro.config.js
+const { getDefaultConfig } = require("expo/metro-config");
+const exclusionList = require("metro-config/src/defaults/exclusionList");
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+// не даём Metro индексировать мусор сборки Android
+config.resolver.blacklistRE = exclusionList([
+  /android\/app\/build\/.*/,
+  /android\/build\/.*/,
+  /android\/\.gradle\/.*/,
+]);
+
+module.exports = config;
